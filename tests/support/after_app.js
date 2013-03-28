96182bc059f75008107adcf2ac95bec902401c55
Drew Purdy <andrewmp1@gmail.com>
2013-03-27 21:47:43 -0700

    Integration test from Bill passes.  I added in the resetApp() call so it works w/ other tests.  Good example of using timeouts to control flow

--- tests/support/after_app.js
+++ tests/support/after_app.js
@@ -1,6 +1,7 @@
 var resetApp = function() {
   Ember.run(function(){
+    if (App.store) App.store.destroy();
+    App.store = App.Store.create();
     App.reset();
   });
-  window.helper = testing(App);
 };
