bd9961888676c066463de6d7341ecfa4b8f02bc5
Scott Smith <scottnelsonsmith@gmail.com>
2013-03-29 19:27:30 -0700

    At this point, testem fails all tests because Ember.Object will no longer 'honor' objects containing methods that do #_super.  Guess what?  Ember data still does _super.  What's weird is that the version of EmberData they are packaging for the newbie zip file is old: revision 4.

--- cp_ember.sh
+++ cp_ember.sh
@@ -1,3 +1,4 @@
 # Convenience script to update our files from our forks of the ember.js project
-cp ../ember.js/dist/ember.js js/libs/
-cp ../ember.js/dist/modules/handlebars.js js/libs/
+cp ../ember.js/dist/ember.js app/js/libs/
+cp ../ember.js/lib/jquery-1.9.0.js app/js/libs/
+cp ../emberdata/dist/ember-data.js app/js/libs/
