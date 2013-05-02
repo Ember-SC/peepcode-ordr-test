f04c1425cd256b2ffe640ec2c9afe1a27851f288
Bill Heaton <bheaton@verticalresponse.com>
2013-05-02 00:59:49 -0700

    Add BDD style suite using pavlov (w/ QUnit) and update helpers
    - Add pavlov to karma config and html spec runner
    - Changed custom testing helper, and used in /tables suite

--- tests/support/test_helper.js
+++ tests/support/test_helper.js
@@ -1,17 +1,23 @@
 var testing = function(app){
-    var container = app.__container__,
-        appController = container.lookup('controller:application'),
-        router = container.lookup("router:main");
 
     var helper = {
+        container: function(){
+            return app.__container__;
+        },
+        appController: function(){
+            return helper.container().lookup('controller:application');
+        },
+        router: function(){
+            return helper.container().lookup('router:main');
+        },
         path: function(){
-            return appController.get('currentPath');
+            return helper.appController().get('currentPath');
         },
         routeName: function(){
             return helper.path().split('.').pop();
         },
         route: function(){
-            return container.lookup('route:' + helper.routeName());
+            return helper.container().lookup('route:' + helper.routeName());
         },
         controller: function(){
             return helper.route().get('controller');
@@ -20,23 +26,24 @@ var testing = function(app){
             return helper.route().get('currentModel');
         },
         transitionTo: function(){
-            return router.transitionTo.apply(router, arguments);
+            return helper.router().transitionTo.apply(helper.router(), arguments);
         },
         send: function(){
-            return router.send.apply(router, arguments);
+            return helper.router().send.apply(helper.router(), arguments);
         },
         lookup: function(path) {
-            return container.lookup(path);
+            return helper.container().lookup(path);
         },
         activeView: function(name) {
-            return router._lookupActiveView(name);
+            return helper.router()._lookupActiveView(name);
         },
         lastSetURL: function(){
-            return router.location.lastSetURL;
+            return helper.router().location.lastSetURL;
         },
         navigateTo: function(url){
-            return router.handleURL(url);
+            return helper.router().handleURL(url);
         }
     };
+
     return helper;
 };
