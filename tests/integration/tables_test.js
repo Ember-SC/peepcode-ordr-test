f0c0be0a14d5215a90709339f0cf94bcfbc56a02
Bill Heaton <pixelhandler@gmail.com>
2013-05-04 02:56:44 -0700

    Clean up specs following change from QUnit test modules to BDD suites
    - Added a few describe statements
    - Moved some text from messages in assertions to the it statements
    - Changed to async testing mode for integration tests

--- tests/integration/tables_test.js
+++ tests/integration/tables_test.js
@@ -1,5 +1,3 @@
-/* globals testing:true */
-
 /*
 var menu = $('#menu li a');
 var tables = $('#tables a');
@@ -7,10 +5,9 @@ var tableNumber = $('div.nine h2 span').text();
 var total = $('#total h3 span').text();
 var addFoodText = $('#customer-tab li h3')[0].firstChild
 */
-
 pavlov.specify("Ordr App integration tests", function(){
 
-  describe("/tables", function () {
+  describe("Tables", function () {
 
     before(function(){
       Ember.run(App, App.advanceReadiness);
@@ -20,20 +17,34 @@ pavlov.specify("Ordr App integration tests", function(){
       App.reset();
     });
 
-    it("should display six tables when launching the app", function () {
-      visit('/').then(function () {
-        assert(testing(App).path()).equals('tables.index', "'/' redirects to '/tables'");
-        assert(find('#tables a').length).equals(6, "There are six tables present.");
-      });
+    describe("Initial state", function(){
+
+      it("should redirect to '/tables'", async(function(){
+        visit('/').then(function () {
+          assert(testing(App).path()).equals('tables.index');
+          resume();
+        });
+      }));
+
+      it("should display six tables", async(function(){
+        visit('/tables').then(function () {
+          assert(find('#tables a').length).equals(6);
+          resume();
+        });
+      }));
     });
 
-    it("should display foods to add to tab on table 2", function () {
-      expect(3);
-      visit('/tables/2').then(function () {
-        assert(find('div.nine h2 span').text()).equals("2", "Table number is 2");
-        assert(find('#customer-tab li h3:first').text()).equals("Click a food to add it", "Placeholder text is visible");
-        assert(find('#menu li > a').length).equals(5, 'Food menu has (5) items.');
-      });
+    describe("Table 2", function () {
+
+      it("should display food menu with 5 choices and placeholder text", async(function(){
+        expect(3);
+        visit('/tables/2').then(function () {
+          assert(find('div.nine h2 span').text()).equals("2");
+          assert(find('#customer-tab li h3:first').text()).equals("Click a food to add it");
+          assert(find('#menu li > a').length).equals(5);
+          resume();
+        });
+      }));
     });
   });
 
--- tests/integration/tabs_test.js
+++ tests/integration/tabs_test.js
@@ -1,6 +1,6 @@
 pavlov.specify("Ordr App integration tests", function(){
 
-  describe("tabs", function () {
+  describe("Tabs", function(){
 
     before(function(){
       Ember.run(App, App.advanceReadiness);
@@ -10,33 +10,43 @@ pavlov.specify("Ordr App integration tests", function(){
       App.reset();
     });
 
-    it("should add pizza to table 1", function () {
-      expect(5);
-      visit('/tables/1').then(function(){
-        assert(find('div.nine h2 span').text()).equals("1", "Table number is 1");
-        return click('#menu li:eq(0) > a');
-      }).then(function(){
-        assert(find('#customer-tab li:eq(0) > h3').text()).equals('Pizza $15.00', 'Pizza added to customer tab.');
-        assert(find('#total span').text()).equals('$15.00', '$15.00 is the total'); // This is wrong but making it pass w/ wrong data.
-        visit('/tables/3').then(function(){
-          assert(find('div.nine h2 span').text()).equals("3", "Table number is 3");
-          visit('/tables/1').then(function(){
-            assert(find('#customer-tab li:eq(0) > h3').text()).equals('Pizza $15.00', 'Pizza still on table 1 tab.');
+    describe("Tables 1 and 3", function(){
+
+      it("should add and keep a $15 pizza in the tab for table 1", async(function(){
+        expect(5);
+        visit('/tables/1').then(function(){
+          assert( find('div.nine h2 span' ).text()).equals( '1' );
+          return click('#menu li:eq(0) > a');
+        }).then(function(){
+          assert( find('#customer-tab li:eq(0) > h3' ).text() ).equals( 'Pizza $15.00' );
+          assert( find('#total span').text() ).equals( '$15.00' ); // This is wrong but making it pass w/ wrong data.
+          visit('/tables/3').then(function(){
+            assert( find('div.nine h2 span').text() ).equals( '3' );
+            visit('/tables/1').then(function(){
+              assert( find('#customer-tab li:eq(0) > h3').text() ).equals( 'Pizza $15.00' );
+              resume();
+            });
           });
         });
-      });
+      }));
     });
 
-    it("should already have foods in tab for table 4", function () {
-      expect(2);
-      var actual = [], expected = 'Pizza$15.00Pancakes$3.00Fries$7.00HotDog$9.50BirthdayCake$20.00Total$54.50';
-      visit('/tables/4').then(function(){
-        find('#customer-tab > li').each(function () {
-          actual.push(find(this).text());
+    describe("Table 4", function(){
+
+      it("should already have $54.50 in foods in tab", async(function(){
+        expect(2);
+        visit('/tables/4').then(function(){
+          var actual = [],
+              expectedFoods = 'Pizza$15.00Pancakes$3.00Fries$7.00HotDog$9.50BirthdayCake$20.00Total$54.50';
+
+          find('#customer-tab > li').each(function(){
+            actual.push( find(this).text() );
+          });
+          assert( actual.join('').replace(/\s/g, '') ).equals( expectedFoods );
+          assert( find('#total span').text() ).equals( '$54.50' );
+          resume();
         });
-        assert(actual.join('').replace(/\s/g, '')).equals(expected, 'table 4 has expected foods in tab.');
-        assert(find('#total span').text()).equals('$54.50', '$54.50 is the total tab for table 4.');
-      });
+      }));
     });
   });
 
